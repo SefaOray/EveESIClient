b05cbef3699304aad78e27dbc8e37d1b091010fe
Sefa Ümit Oray <sefa@fastmail.com>
2018-02-15 22:08:52 +0300

    Incursions endpoints added

---
+++ EveESIClient.Models/Incursions/ListIncursionsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EveESIClient.Models.Incursions
+{
+    public class ListIncursionsResponse
+    {
+        public string Type { get; set; }
+        /// <summary>
+        /// [ withdrawing, mobilizing, established ]
+        /// </summary>
+        public string State { get; set; }
+        public float Influence { get; set; }
+        public bool Has_boss { get; set; }
+        public int Faction_id { get; set; }
+        public int Constellation_id { get; set; }
+        public int Staging_solar_system_id { get; set; }
+        public List<int> Infested_solar_systems { get; set; }
+    }
+}
---
+++ EveESIClient/Incursions/IncursionsClient.cs
@@ -0,0 +1,27 @@
+﻿using EveESIClient.Client;
+using EveESIClient.Helpers;
+using EveESIClient.Models.Incursions;
+using System.Collections.Generic;
+
+namespace EveESIClient.Incursions
+{
+    public class IncursionsClient
+    {
+        private readonly IHttpClient _httpClient;
+        internal IncursionsClient(IHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Return a list of current incursions
+        /// </summary>
+        /// <returns>A list of incursions</returns>
+        public ESIResponse<List<ListIncursionsResponse>> ListIncursions()
+        {
+            var request = RestRequestHelper.CreateGetRequest("incursions/");
+
+            return _httpClient.Execute<List<ListIncursionsResponse>>(request);
+        }
+    }
+}
